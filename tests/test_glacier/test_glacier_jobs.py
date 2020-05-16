b17bf9e9299511458dda45f18119fce01ec9a45b
Steve Pulec <spulec@gmail.com>
2020-05-16 17:38:52 -0500

    Remove mock_glacier_deprecated.

--- tests/test_glacier/test_glacier_jobs.py
+++ tests/test_glacier/test_glacier_jobs.py
@@ -3,89 +3,88 @@ from __future__ import unicode_literals
 import json
 import time
 
-from boto.glacier.layer1 import Layer1
+import boto3
 import sure  # noqa
 
-from moto import mock_glacier_deprecated
+from moto import mock_glacier
 
 
-@mock_glacier_deprecated
+@mock_glacier
 def test_init_glacier_job():
-    conn = Layer1(region_name="us-west-2")
+    conn = boto3.client("glacier", region_name="us-west-2")
     vault_name = "my_vault"
-    conn.create_vault(vault_name)
+    conn.create_vault(vaultName=vault_name)
     archive_id = conn.upload_archive(
-        vault_name, "some stuff", "", "", "some description"
-    )
+        vaultName=vault_name, body="some stuff", checksum="", archiveDescription="some description"
+    )['archiveId']
 
     job_response = conn.initiate_job(
-        vault_name, {"ArchiveId": archive_id, "Type": "archive-retrieval"}
+        vaultName=vault_name, jobParameters={"ArchiveId": archive_id, "Type": "archive-retrieval"}
     )
-    job_id = job_response["JobId"]
-    job_response["Location"].should.equal("//vaults/my_vault/jobs/{0}".format(job_id))
+    job_id = job_response["jobId"]
+    job_response["location"].should.equal("//vaults/my_vault/jobs/{0}".format(job_id))
 
 
-@mock_glacier_deprecated
+@mock_glacier
 def test_describe_job():
-    conn = Layer1(region_name="us-west-2")
+    conn = boto3.client("glacier", region_name="us-west-2")
     vault_name = "my_vault"
-    conn.create_vault(vault_name)
+    conn.create_vault(vaultName=vault_name)
     archive_id = conn.upload_archive(
-        vault_name, "some stuff", "", "", "some description"
-    )
+        vaultName=vault_name, body="some stuff", checksum="", archiveDescription="some description"
+    )['archiveId']
     job_response = conn.initiate_job(
-        vault_name, {"ArchiveId": archive_id, "Type": "archive-retrieval"}
+        vaultName=vault_name, jobParameters={"ArchiveId": archive_id, "Type": "archive-retrieval"}
     )
-    job_id = job_response["JobId"]
+    job_id = job_response["jobId"]
 
-    job = conn.describe_job(vault_name, job_id)
-    joboutput = json.loads(job.read().decode("utf-8"))
+    job = conn.describe_job(vaultName=vault_name, jobId=job_id)
 
-    joboutput.should.have.key("Tier").which.should.equal("Standard")
-    joboutput.should.have.key("StatusCode").which.should.equal("InProgress")
-    joboutput.should.have.key("VaultARN").which.should.equal(
+    job.should.have.key("Tier").which.should.equal("Standard")
+    job.should.have.key("StatusCode").which.should.equal("InProgress")
+    job.should.have.key("VaultARN").which.should.equal(
         "arn:aws:glacier:us-west-2:012345678901:vaults/my_vault"
     )
 
 
-@mock_glacier_deprecated
+@mock_glacier
 def test_list_glacier_jobs():
-    conn = Layer1(region_name="us-west-2")
+    conn = boto3.client("glacier", region_name="us-west-2")
     vault_name = "my_vault"
-    conn.create_vault(vault_name)
+    conn.create_vault(vaultName=vault_name)
     archive_id1 = conn.upload_archive(
-        vault_name, "some stuff", "", "", "some description"
-    )["ArchiveId"]
+        vaultName=vault_name, body="some stuff", checksum="", archiveDescription="some description"
+    )["archiveId"]
     archive_id2 = conn.upload_archive(
-        vault_name, "some other stuff", "", "", "some description"
-    )["ArchiveId"]
+        vaultName=vault_name, body="some other stuff", checksum="", archiveDescription="some description"
+    )["archiveId"]
 
     conn.initiate_job(
-        vault_name, {"ArchiveId": archive_id1, "Type": "archive-retrieval"}
+        vaultName=vault_name, jobParameters={"ArchiveId": archive_id1, "Type": "archive-retrieval"}
     )
     conn.initiate_job(
-        vault_name, {"ArchiveId": archive_id2, "Type": "archive-retrieval"}
+        vaultName=vault_name, jobParameters={"ArchiveId": archive_id2, "Type": "archive-retrieval"}
     )
 
-    jobs = conn.list_jobs(vault_name)
+    jobs = conn.list_jobs(vaultName=vault_name)
     len(jobs["JobList"]).should.equal(2)
 
 
-@mock_glacier_deprecated
+@mock_glacier
 def test_get_job_output():
-    conn = Layer1(region_name="us-west-2")
+    conn = boto3.client("glacier", region_name="us-west-2")
     vault_name = "my_vault"
-    conn.create_vault(vault_name)
+    conn.create_vault(vaultName=vault_name)
     archive_response = conn.upload_archive(
-        vault_name, "some stuff", "", "", "some description"
+        vaultName=vault_name, body="some stuff", checksum="", archiveDescription="some description"
     )
-    archive_id = archive_response["ArchiveId"]
+    archive_id = archive_response["archiveId"]
     job_response = conn.initiate_job(
-        vault_name, {"ArchiveId": archive_id, "Type": "archive-retrieval"}
+        vaultName=vault_name, jobParameters={"ArchiveId": archive_id, "Type": "archive-retrieval"}
     )
-    job_id = job_response["JobId"]
+    job_id = job_response["jobId"]
 
     time.sleep(6)
 
-    output = conn.get_job_output(vault_name, job_id)
+    output = conn.get_job_output(vaultName=vault_name, jobId=job_id)['body']
     output.read().decode("utf-8").should.equal("some stuff")
--- tests/test_glacier/test_glacier_vaults.py
+++ tests/test_glacier/test_glacier_vaults.py
@@ -1,31 +1,31 @@
 from __future__ import unicode_literals
 
-import boto.glacier
+import boto3
 import sure  # noqa
 
-from moto import mock_glacier_deprecated
+from moto import mock_glacier
 
 
-@mock_glacier_deprecated
+@mock_glacier
 def test_create_vault():
-    conn = boto.glacier.connect_to_region("us-west-2")
+    conn = boto3.client("glacier", region_name="us-west-2")
 
-    conn.create_vault("my_vault")
+    conn.create_vault(vaultName="my_vault")
 
-    vaults = conn.list_vaults()
+    vaults = conn.list_vaults()['VaultList']
     vaults.should.have.length_of(1)
-    vaults[0].name.should.equal("my_vault")
+    vaults[0]['VaultName'].should.equal("my_vault")
 
 
-@mock_glacier_deprecated
+@mock_glacier
 def test_delete_vault():
-    conn = boto.glacier.connect_to_region("us-west-2")
+    conn = boto3.client("glacier", region_name="us-west-2")
 
-    conn.create_vault("my_vault")
+    conn.create_vault(vaultName="my_vault")
 
-    vaults = conn.list_vaults()
+    vaults = conn.list_vaults()['VaultList']
     vaults.should.have.length_of(1)
 
-    conn.delete_vault("my_vault")
-    vaults = conn.list_vaults()
+    conn.delete_vault(vaultName="my_vault")
+    vaults = conn.list_vaults()['VaultList']
     vaults.should.have.length_of(0)
